7d92c35f5ca80bceb12730ba32edf3b2fc586814
ChromMob <filip.chromy04@gmail.com>
2022-06-06 18:36:01 +0200

    Minestore sponge initial commit - command delivery working - command storing if player offline working - other features will maybe get implemented later

--- src/main/java/me/chrommob/minestore/commands/PunishmentManager.java
+++ src/main/java/me/chrommob/minestore/commands/PunishmentManager.java
@@ -2,19 +2,19 @@ package me.chrommob.minestore.commands;
 
 import me.chrommob.minestore.MineStore;
 import me.chrommob.minestore.commandexecution.Command;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class PunishmentManager extends JavaPlugin {
-    static File later = new File(MineStore.instance.getDataFolder()+"/later.yml");
+public class PunishmentManager {
+    static File folder = new File(String.valueOf(MineStore.instance.getPrivateConfigDir()));
+    static File later = new File(MineStore.instance.getPrivateConfigDir() +"/later.yml");
     static Yaml yaml = new Yaml();
     public static void create() {
         try {
+            folder.mkdirs();
             later.createNewFile();
         } catch (Exception e){
             e.printStackTrace();
@@ -38,9 +38,9 @@ public class PunishmentManager extends JavaPlugin {
             }
             if (Command.runLater == null){
                 Command.runLater = new HashMap<>();
-                Bukkit.getLogger().info("[MineStore] Command file empty.");
+                MineStore.instance.getLogger().info("[MineStore] Command file empty.");
             } else {
-                Bukkit.getLogger().info("[MineStore] Loaded commands for " + Command.runLater.size() + " players.");
+                MineStore.instance.getLogger().info("[MineStore] Loaded commands for " + Command.runLater.size() + " players.");
             }
         } catch (Exception e){
             e.printStackTrace();
